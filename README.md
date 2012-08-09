c4b6f50aa3bf5eab29838360a69719e44702e023
reu-res <rabotnik.n1@gmail.com>
2012-08-09 14:14:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+#Sublime Text 2 Assistant
+
+This script allows you to get a little more information about the state of Sublime Text 2, you can use when writing their own plugins.
+
+##Instalation
+You can copy `ST2Assistant.py` to directory when instaled Sublime Text 2, or to **User** package directory.
+
+The **User** package directory is located at:
+
+* OS X:
+
+        ~/Library/Application Support/Sublime Text 2/Packages/User
+
+* Linux:
+
+        ~/.config/sublime-text-2/Packages/User
+
+* Windows:
+
+        %APPDATA%/Sublime Text 2/Packages/User
+
+    If you use portable version:
+
+        <Sublime Text 2 directory>/Data/Packages/User
+
+##Usage
+`import ST2Assistant
+...
+assistant = ST2Assistant.assistant()
+if assistant.is_side_bar_visible():
+    print 'Side bar is visible'
+else:
+    print 'Side bar is hidden'`
+
+##Functions
+[List of functions](wiki/functions)
---
+++ ST2Assistant.py
@@ -0,0 +1,51 @@
+try:
+    import sublime
+except ImportError, e:
+    raise e
+import os
+import json
+
+AUTO_SAVE_SESSION = u'Settings/Auto Save Session.sublime_session'
+SESSION = u'Settings/Session.sublime_session'
+
+
+class Assistant():
+    def __init__(self):
+        self.st_path, _ = os.path.split(sublime.packages_path())
+        self.get_session()
+
+    def get_session(self):
+        path = os.path.join(self.st_path, AUTO_SAVE_SESSION)
+        if os.path.exists(path):
+            with open(path) as f:
+                session_data = f.read()
+        elif os.path.exists(os.path.join(self.st_path, SESSION)):
+            with open(os.path.join(self.st_path, SESSION)) as f:
+                session_data = f.read()
+        else:
+            raise IOError('Session not found')
+
+        js = json.JSONDecoder(strict=False)
+        self.session = js.decode(session_data)
+
+    def is_portable(self):
+        _, _dir = os.path.split(self.st_path)
+        return _dir == u'Data'
+
+    def platform(self):
+        return sublime.platform()
+
+    def version(self):
+        return sublime.version()
+
+    def arch(self):
+        return sublime.arch()
+
+    def is_minimap_visible(self):
+        return self.session['windows'][0]['show_minimap']
+
+    def is_status_bar_visible(self):
+        return self.session['windows'][0]['status_bar_visible']
+
+    def is_side_bar_visible(self):
+        return self.session['windows'][0]['side_bar_visible']
